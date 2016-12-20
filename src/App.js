d5831b311520a95dde44e6b70a67a337a0035d7b
Lucas K Allmon <lucaskallmon@gmail.com>
2016-12-20 14:20:26 -0800

    feature(product list): populate product previews
    Get the product list running. Map the objects in Products to the ProductCard functional component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ProductCard from './ProductCard';
+
+//Normally an API call on app initialization , that I would populate to sessionStorage, or Redux 
+import products from './products';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { productList: products.documents }
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Get Started</h1>
-      </div>
+      <section className='product-list flex flex-wrap vh-100 w-100 mt5 pt2'>
+        {this.state.productList.map(el => (
+          <ProductCard
+            colors={el.color_en}
+            displayName={el.productDisplayName_en}
+            images={el.images}
+            key={el.productNo}
+            inStock={el.inStock_US_CONSUMER}
+            productNumber={el.productNo}
+            skuName={el.skuDisplayName_en[0]}
+          />
+        ))}
+      </section>
     );
   }
 }
---
+++ src/ProductCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ImageFallback from 'react-image-fallback';
+import products from './products';
+
+const ProductCard = (props) => (
+  <article className="bg-white w-50-m w-25-l ph2 pb3 flex flex-column items-center">
+    <div className="ba b--silver br2">
+      <h2 className="f4 fw1 title ph2 tc">
+        {props.displayName ?  props.displayName : props.skuName }
+      </h2>
+      <ImageFallback
+        className='bt bb b--light-gray w-100'
+        src={props.images[0]}
+        fallbackImage='http://uploads.fender.com/newsroom/logos/fender-red-large.jpg'
+        initialImage='http://uploads.fender.com/newsroom/logos/fender-red-large.jpgf'
+        alt='Product Image'
+       />
+       <div className="ph2">
+         <h3 className="f4 fw1"><span className="b">Availability:</span>
+           {props.inStock === 'inStock' ?
+             <span className="green"> In Stock</span> :
+             <span className="dark-red"> Out Of Stock</span>
+           }
+         </h3>
+          <h4 className="fw1">
+          Color(s): {props.colors.map(color => {
+            if (props.colors.length === 1) {
+              return <span key={color}>{color} </span>
+            } else {
+              return <span key={color}>{color}, </span>
+            }
+           })}
+         </h4>
+         <h4 className="fw1">Product No: {props.productNumber}</h4>
+         <h4 className="fw1">UPC: 717669068455</h4>
+       </div>
+    </div>
+  </article>
+);
+
+export default ProductCard;
